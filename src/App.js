5fea574baef87e0a512dfd1df5ce69ca0968c890
Mervin-Song <mervinsong.wj@gmail.com>
2023-03-28 16:10:57 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let image =
+    "https://scontent.fsin3-1.fna.fbcdn.net/v/t1.18169-9/420663_1943625406307_1444827188_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=de6eea&_nc_ohc=kk7BuurEgX8AX8PwKBc&_nc_ht=scontent.fsin3-1.fna&oh=00_AfAxmawk23sD6nlmkhL8RdCaTwlNIrtoDbqY-lnc_Fcy9w&oe=644A1792";
+
+  const colors = [
+    "#ff0000",
+    "#ff7f00",
+    "#ffff00",
+    "#00ff00",
+    "#0000ff",
+    "#4b0082",
+    "#8f00ff",
+  ];
+  let currentColorIndex = 0;
+
+  function changeBackgroundColor() {
+    const body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = colors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+  }
+
+  setInterval(changeBackgroundColor, 3000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="image-container">
+      <h1>HAPPY BIRTHDAY CARRIE!!!</h1>
+      <img src={image} alt="Image 1" />
     </div>
   );
 }
